79cbdd8c864b87acad127750e79a72f0001a54a5
Andrew Grenon <andrew.grenon@gmail.com>
2023-01-22 16:52:15 +1300

    Done - v.1 ready for deploy

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -21,8 +23,13 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>{`Opera singer turned front end web developer (React, Next.js, Tailwind, MUI)`}</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a basic blog app built on React with Next.js which covers
+          static generation, server-side rendering, and pre-rendering.{" "}
+          <a href="https://nextjs.org/learn">
+            Click here to learn Next.js, a powerful framework for creating
+            powerfully performant React apps.
+          </a>
+          .
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -30,11 +37,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
